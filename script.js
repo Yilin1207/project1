f1583763c3f81f2748899ff5ec4a4b030febe22c
Nureke <nyeleusiz@bk.ru>
2026-09-10 16:57:49 +0500

    added script to js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function changeBg() {
-    const colors = ['#1a1a1a', '#2c3e50', '#0f2027', '#200122'];
+    const colors = ['#1a1a1a', '#2c3e51', '#0f2027', '#200122'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.body.style.backgroundColor = randomColor;
 }
